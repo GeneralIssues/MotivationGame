1c60adc94624adc1fcb2246a2a69522551f267a7
Holt95 <patrick1995holt@gmail.com>
2017-04-04 19:37:46 +0200

    Canvas and script work

---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleController : MonoBehaviour {
+
+    public static int puzzleSize = 1;
+    public Image[,] images = new Image[puzzleSize, puzzleSize];
+
+	// Use this for initialization
+	void Start () {
+        
+        for (int y = 0; y < puzzleSize; y++) {
+            for (int x = 0; x < puzzleSize; x++) {
+                
+            }
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+            images[0, 0].GetComponent<Image>().color = Color.black;
+        }
+	}
+}
