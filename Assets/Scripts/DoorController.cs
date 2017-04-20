c0d8feb1c50af118402328f916b89a203b28436b
Fred <felmqu14@student.aau.dk>
2017-04-20 12:12:39 +0200

    Level setup, first iteration

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -14,18 +14,32 @@ public class DoorController : MonoBehaviour{
 	// Update is called once per frame
 	void Update () {
 		
+        /*
+         * if door is touched and click Space, open puzzle
+         * If puzzle is solved, play door open animation at and make collider a trigger
+         * When trigger is triggered, move camera and player
+         * 
+         * */
 	}
 
     void OnTriggerEnter2D(Collider2D coll){
         if (coll.tag == "Player"){
             if (this.name == "Door1"){
-                cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("1Cam"));
+                Instantiate(PrefabManager)
+
+                //cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("1Cam"));
             }else if (this.name == "Door2"){
-                cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("2Cam"));
+
+
+                //cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("2Cam"));
             }else if (this.name == "Door3"){
-                cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("3Cam"));
+
+
+                //cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("3Cam"));
             }else if (this.name == "Door4"){
-                cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("4Cam"));
+
+
+                //cc.GetComponent<CameraController>().MoveCamToRoom(GameObject.Find("4Cam"));
             }
         }
     }
