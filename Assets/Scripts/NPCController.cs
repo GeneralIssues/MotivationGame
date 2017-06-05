b4a0114aaa4225cfe17e5dcf92372ddccd4bae26
Fred <felmqu14@student.aau.dk>
2017-06-05 11:16:27 +0200

    NPCcontroller changes

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -15,14 +15,25 @@ public class NPCController : MonoBehaviour {
     private bool three;
     private bool four;
     private bool five;
+    private bool six;
+    private bool seven;
+    private bool eight;
+    private bool nine;
+    private bool ten;
     private bool currentlyTalking;
 
     //Lore strings
-    private string[] lore1 = { "loreeee", "ormeoie" };
-    private string[] lore2 = { "Some more lore", "Stupid poopie", "placeholder" };
-    private string[] lore3 = { "lalalala", "Stupid poopie", "placeholder" };
-    private string[] lore4 = { "askmdks", "Stupid poopie", "placeholder" };
-    private string[] lore5 = { "Soasdasdsaade", "Stupiasdasdopie", "asdasdlder" }; 
+    private string[] lore1 = { "Log 1, Stardate 237: The reactor shut down…", "… our systems powered down, one by one…", "...most of the crew ejected…", "..but the rest are stranded here!" };
+    private string[] lore2 = { "Log 2, Stardate 238: The crew consisted of over 100 people…", "...the lifepods only had room for 65 of them…", "...there was a panic, people stampeded…", "...those who made it escaped…", "...we few others were left here to die!" };
+    private string[] lore3 = { "Log 3, Stardate 239: Our life support is failing…", "...the oxygen levels are slowly degrading…", "...we sit watching, praying…", "...huddled together for warmth…", "...space is a silent and cold killer!" };
+    private string[] lore4 = { "Log 4, Stardate 240: Diane went crazy…", "...said he couldn’t stand the waiting…", "...went out the airlock without a suit…", "...microgravity is making his corpse stick to the ship!" };
+    private string[] lore5 = { "Log 5, Stardate 240: Diane’s husband Morty…", "...he’s gone, and so is our last exo-suit…", "...we think he might have gone to get Diane…", "...who knows if he’ll be back!" };
+    private string[] lore6 = { "Log 6, Stardate 241: We found some more chlorite candles…", "...they should give us a few more days…", "...Rick didn’t want us to use them…", "...says it’s pointless to try…", "...we’re all losing hope!" };
+    private string[] lore7 = { "Log 7, Stardate 241: Rick had to be subdued…", "...he refused to let us light the candles…", "...he’s locked in the brig now…", "...his screams are echoing the ship!" };
+    private string[] lore8 = { "Log 8, Stardate 242: We’re down to our last litre of water…", "...seems lack of oxygen might not be our death after all…", "...we’re all so thirsty, but we have to ration it…", "...Tricia keeps talking about “the rescue soon to come”...", "...as if anyone will come for us!" };
+    private string[] lore9 = { "Log 9, Stardate 244: Rick hung himself in the brig…", "...we’re out of water, and food too...", "...maybe he is the lucky one!" };
+    private string[] lore10 = { "Log 10: stardate 247: The oxygen is almost gone…", "...so is everyone else…", "...but at least I have plenty of food and drink now…", "...my mouth constantly tastes like iron…", "...if anyone finds this log…", "...pray for my soul!" };
+
 
 
     // Use this for initialization
@@ -79,6 +90,46 @@ public class NPCController : MonoBehaviour {
                 five = true;
 
             }
+            else if (this.name == "NPC6" && !six)
+            {
+                StartCoroutine(DialogueStart(lore6));
+                mc.GetComponent<MotivationController>().IncreaseImmersionScore(5);
+                pm.GetComponent<PrefabManager>().list.RemoveAt(5);
+                six = true;
+
+            }
+            else if (this.name == "NPC7" && !seven)
+            {
+                StartCoroutine(DialogueStart(lore7));
+                mc.GetComponent<MotivationController>().IncreaseImmersionScore(5);
+                pm.GetComponent<PrefabManager>().list.RemoveAt(6);
+                seven = true;
+
+            }
+            else if (this.name == "NPC8" && !eight)
+            {
+                StartCoroutine(DialogueStart(lore8));
+                mc.GetComponent<MotivationController>().IncreaseImmersionScore(5);
+                pm.GetComponent<PrefabManager>().list.RemoveAt(7);
+                eight = true;
+
+            }
+            else if (this.name == "NPC9" && !nine)
+            {
+                StartCoroutine(DialogueStart(lore9));
+                mc.GetComponent<MotivationController>().IncreaseImmersionScore(5);
+                pm.GetComponent<PrefabManager>().list.RemoveAt(8);
+                nine = true;
+
+            }
+            else if (this.name == "NPC10" && !ten)
+            {
+                StartCoroutine(DialogueStart(lore10));
+                mc.GetComponent<MotivationController>().IncreaseImmersionScore(5);
+                pm.GetComponent<PrefabManager>().list.RemoveAt(9);
+                ten = true;
+
+            }
         }
     }
 
@@ -87,9 +138,7 @@ public class NPCController : MonoBehaviour {
 
         foreach (string text in texts)
         {
-
             yield return StartCoroutine(Dialogue(text));
-
         }
         npctext.text = "";
         currentlyTalking = false;
@@ -99,7 +148,7 @@ public class NPCController : MonoBehaviour {
     IEnumerator Dialogue(string text)
     {
         npctext.text = text;
-        yield return StartCoroutine(WaitForKeyDown(KeyCode.Z));
+        yield return StartCoroutine(WaitForKeyDown(KeyCode.E));
     }
     IEnumerator WaitForKeyDown(KeyCode keyCode)
     {
